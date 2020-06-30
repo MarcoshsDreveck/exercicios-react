900a1ad8d88aed2ef0b01e7b6d7aaf68553513f3
Marcos Henrique Stuber Dreveck <ja31101@malwee.com.br>
2020-06-30 08:45:37 -0300

    ajuste todo

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+
+import App from './main/app'
 
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
